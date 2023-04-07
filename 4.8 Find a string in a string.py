a068dee85930c535092a733c0b397b0056f0c62d
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-04-07 18:36:40 +0200

    Solved exercisess

---
+++ 4.8 Find a string in a string.py
@@ -0,0 +1,5 @@
+phrase = "the surprise is in here somewhere"
+print(phrase.find("surprise"))
+
+string2 = "My name is Wojciech"
+print(string2.replace("Wojciech", "Mateusz"))
---
+++ 4.8 Review exercises.py
@@ -0,0 +1,12 @@
+# 1
+string1 = "AAA"
+print(string1.find("a"))
+
+# 2
+n = 2.0
+var = input("Enter a string: ")
+print(var.find(str(n)))
+
+# 3
+input1 = input("Enter a string: ")
+print(input1.find("a"))
