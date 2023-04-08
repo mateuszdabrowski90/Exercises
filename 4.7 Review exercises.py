3ecf918fc5f77ec2cf2c7cd898975f03083e9517
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-04-08 07:37:02 +0200

    Solved exercisess v2

--- 4.7 Review exercises.py
+++ 4.7 Review exercises.py
@@ -1,7 +1,7 @@
 # 1
 weight = 0.2
 animal = "newt"
-print(f"{weight} kg is the weight of the {animal}")
+print(str(weight) + " kg is the weight of the " + animal)
 
 # 2
 print("{} kg is the weight of the {}".format(weight, animal))
