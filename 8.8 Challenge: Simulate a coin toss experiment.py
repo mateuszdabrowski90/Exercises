9507d17c1d244f534fac17c52b5c1d80bf7bf5be
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-04-19 21:26:26 +0200

    Solved exercises

---
+++ 8.8 Challenge: Simulate a coin toss experiment.py
@@ -0,0 +1,14 @@
+import random
+
+head = 0
+tail = 0
+flip = 0
+position_s = random.randint(1, 2)
+for i in range(10000):
+    position_n = random.randint(1, 2)
+    if position_n == position_s:
+        continue
+    else:
+        flip = flip + 1
+        position_n = position_s
+print(flip +1)
