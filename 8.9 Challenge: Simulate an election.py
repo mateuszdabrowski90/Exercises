fabe4d7aae7bed5cdfd37188f9a525b6e01efdf1
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-04-24 21:23:19 +0200

    Solved exercises

---
+++ 8.9 Challenge: Simulate an election.py
@@ -0,0 +1,16 @@
+import random
+
+cand_a = 0
+cand_b = 0
+def election():
+    cand_a = 0
+    cand_b = 0
+    for i in range(10000):
+        x = random.randint(1, 2)
+        if x == 1:
+            cand_a = cand_a + 1
+        else:
+            cand_b = cand_b + 1
+
+regions = 3
+
