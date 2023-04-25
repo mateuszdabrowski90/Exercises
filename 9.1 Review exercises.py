bef0019b1d87bf3fdbbb180101785669f4af5bf6
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-04-25 17:30:30 +0200

    Solved exercises

---
+++ 9.1 Review exercises.py
@@ -0,0 +1,19 @@
+# 1
+cardinal_numbers = ("first", "third", "second")
+
+# 2
+print(cardinal_numbers[1])
+
+# 3
+position1, position2, position3 = cardinal_numbers
+for i in cardinal_numbers:
+    print(i)
+
+# 4
+my_name = tuple("Mateusz")
+
+# 5
+print("x" in my_name)
+
+# 6
+print(my_name[1:])
