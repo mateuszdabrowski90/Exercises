d15a38d492050b06900ce7e32f0c559f5174d8b9
mateuszdabrowski90 <mateusz.w.dabrowski@gmail.com>
2023-06-13 17:07:17 +0200

    Solved exercises

---
+++ 9.2 Review exercises.py
@@ -0,0 +1,22 @@
+# 1
+food = ["rice", "beans"]
+
+# 2
+food.append("brocoli")
+
+# 3
+food.extend(["bread", "pizza"])
+
+# 4
+print(food[0:2])
+
+# 5
+print(food[-1:])
+
+# 6
+products = "eggs, fruit, orange juice"
+breakfast = products.split(", ")
+print(breakfast)
+
+# 7
+print(len(breakfast) == 3)
